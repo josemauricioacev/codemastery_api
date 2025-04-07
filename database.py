5ea19acd473e920394895c832a352f9e7ca6a680
josemauricioacev <122044542@upq.edu.mx>
2025-04-06 19:10:15 -0600

    correciones

--- database.py
+++ database.py
@@ -9,6 +9,7 @@ load_dotenv(dotenv_path=env_path)
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),  # ¡Esto es esencial!
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
